8ebddda2eb193278ea796410646e51d26b67a722
Jeremiah Egwa <jerriee232@gmail.com>
2024-07-05 01:18:26 +0100

    completed a simple server/API in Node with Express.js

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
-let espress = require("express");
-let app = espress();
+let express = require("express");
+let app = express();
+
+app.get("/", function (request, response) {
+  response.send("My First API");
+});
+
+app.get("/snacks", function (req, res) {
+  res.send("Hey get me some Snacks...!");
+});
+
+app.listen(3000, function () {
+  console.log("First API running on port 3000!!");
+});
