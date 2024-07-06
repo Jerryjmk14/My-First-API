da0353c011de6d98f282cf7274aa257c482b0eec
Jeremiah Egwa <jerriee232@gmail.com>
2024-07-07 00:00:18 +0100

    added a post request

--- server.js
+++ server.js
@@ -32,6 +32,16 @@ app.get("/snacks", function (req, res) {
   res.send("Hey get me some Snacks...!");
 });
 
+app.post("/", function (req, res) {
+  let ingredient = res.body;
+  if (!ingredient || ingredient === "") {
+    res.status(500).send({ error: "Your ingredient must have text" });
+  } else {
+    ingredients.push(ingredient);
+    res.status(200).send(ingredients);
+  }
+});
+
 app.listen(3000, function () {
   console.log("First API running on port 3000!!");
 });
