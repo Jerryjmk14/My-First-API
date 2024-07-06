6e869fee47c8869f908e7c7f3b11c4af8d7e72fc
Jeremiah Egwa <jerriee232@gmail.com>
2024-07-06 23:46:08 +0100

    modified server to return a response with list of ingredients

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 let express = require("express");
 let app = express();
 
+let bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+let ingredients = [
+  {
+    id: "24kjd4",
+    text: "Eggs",
+  },
+  {
+    id: "464nkmd",
+    text: "Milk",
+  },
+  {
+    id: "mndj578",
+    text: "Mango",
+  },
+  {
+    id: "2aks45",
+    text: "Apples",
+  },
+];
+
 app.get("/", function (request, response) {
-  response.send("My First API");
+  response.send(ingredients);
 });
 
 app.get("/snacks", function (req, res) {
